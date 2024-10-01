1d4f3e1957b537a9076e526ad71729b677dab9ce
mouadmakboul <mouadmakboul8@gmail.com>
2024-10-02 00:28:41 +0200

    last commit

--- src/main/java/com/example/metrics/repository/ClientRepository.java
+++ src/main/java/com/example/metrics/repository/ClientRepository.java
@@ -2,6 +2,9 @@ package com.example.metrics.repository;
 
 import com.example.metrics.entity.clientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-//public interface ClientRepository extends JpaRepository<clientEntity, Long> {
-//}
+@Repository
+
+public interface ClientRepository extends JpaRepository<clientEntity, Long> {
+}
